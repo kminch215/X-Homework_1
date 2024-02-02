c157474886f17eed18c420879b5fd06ef6042ab2
sarahkgreenwood <149001674+sarahkgreenwood@users.noreply.github.com>
2024-02-01 21:27:43 -0600

    Homework_1.java

--- src/edu/mu/Main.java
+++ src/edu/mu/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Kendra Minch: 113387890");
+		System.out.println("Sarah Greenwood" + "\n" + "skgreenwood21");
 	}
 }
