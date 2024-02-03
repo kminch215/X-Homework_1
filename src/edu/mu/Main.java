8d37a60a251b66dad7eb5d04eb8f9e29237ec8cd
LPCardinals <116538923+LPCardinals@users.noreply.github.com>
2024-02-02 20:12:32 -0600

    Update Main.java

--- src/edu/mu/Main.java
+++ src/edu/mu/Main.java
@@ -6,5 +6,6 @@ public class Main {
 		System.out.println("Kendra Minch: 113387890");
 		System.out.println("Sarah Greenwood" + "\n" + "149001674");
 		System.out.println("Samantha Whitaker: 126726363");
+		System.out.println("Lane Parker: 116538923);
 	}
 }
